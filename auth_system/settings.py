90060b8637d25a8c390fd3e2d68d68a4490730cd
austin0x1A4 <karurimm@gmail.com>
2024-05-29 00:33:02 +0300

    added account_balance view

--- auth_system/settings.py
+++ auth_system/settings.py
@@ -82,13 +82,8 @@ WSGI_APPLICATION = 'auth_system.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-'''
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.db.sqlite3',
@@ -99,7 +94,7 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
-'''
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
